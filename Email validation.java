302f015957f63d065437bc0daee67eb2eda24fb7
Sharath <sharath9823@gmail.com>
2025-12-02 14:36:39 +0530

    Create Email validation.java

---
+++ Email validation.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+// Custom Exception
+class InvalidEmailException extends Exception {
+    public InvalidEmailException(String message) {
+        super(message);
+    }
+}
+
+public class EmailValidation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter email: ");
+        String email = sc.nextLine();
+
+        try {
+            validateEmail(email);
+            System.out.println("Valid email!");
+        } catch (InvalidEmailException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        sc.close();
+    }
+
+    public static void validateEmail(String email) throws InvalidEmailException {
+        if (!email.contains("@")) {
+            throw new InvalidEmailException("Email must contain '@' symbol!");
+        }
+    }
+}
